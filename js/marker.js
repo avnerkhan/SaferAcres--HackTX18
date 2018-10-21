fd8657ae8259472ff8d1598e5c09ae2c80d4433b
Jerry Huang <jerry@aplomb.co>
2018-10-20 22:15:28 -0500

    filtering btiches

--- js/marker.js
+++ js/marker.js
@@ -1,9 +1,55 @@
 
 var pins = []
 var popups = []
+var predictPins = []
+var isPredicted = false
+
+function toggle(){
+  isPredicted = !isPredicted;
+  var pins = JSON.parse(sessionStorage.getItem("predictPins"))
+  if (isPredicted){
+    console.log("show apd and prediction pins...")
+    var layerName = "pred-pin";
+    map.addPins(pins, {
+      fontColor: "#000",
+      fontSize: 14,
+      icon: "pin-blue",
+      iconSize: 1,
+      name: layerName,
+      textFont: "SegoeUi-Bold",
+      textOffset: [0, 0],
+      title: ""
+    });
+  }
+  else {
+    console.log("show apd pins only...")
+    map.removeLayers(["pred-pin"])
+  }
+
+
+}
 
 function loadData(){
   console.log("loading all data...")
+  $.get("http://localhost:5000/predictalccrimes", function(data){
+    console.log("Prediction data....")
+    console.log(data)
+
+    for(var i = 0; i<data.length; i++) {
+      var lat = data[i].latitude
+      var long = data[i].longitude
+      var pos = [long, lat]
+      var pin = new atlas.data.Feature(new atlas.data.Point(pos));
+      predictPins.push(pin)
+    }
+
+    var predictPinsString = JSON.stringify(predictPins)
+
+    sessionStorage.setItem("predictPins", predictPinsString)
+
+
+  })
+
   $.get("http://localhost:5000/allcrimes", function(data){
 
     console.log(data)
@@ -17,73 +63,45 @@ function loadData(){
       var pos = [data[i].crime_long, data[i].crime_lat]
 
       var pin = new atlas.data.Feature(new atlas.data.Point(pos));
-      console.log("This is date")
+      // console.log("This is date")
       var crimeDate = new Date(date);
       var todayDate = new Date();
-      console.log(crimeDate.getDay())
+      // console.log(crimeDate.getDay())
 
 
       if(crimeDate.getDay() == todayDate.getDay() ){
       pins.push(pin)
       }
-      /* Create content for popup */
-      var popupContentElement = document.createElement("div");
-      popupContentElement.style.padding = "8px";
-      var popupNameElement = document.createElement("div");
-      popupNameElement.innerText = crimeName;
-      popupContentElement.appendChild(popupNameElement);
 
-      /* Create a popup */
-      var popup = new atlas.Popup({
-        content: popupContentElement,
-        position: pos,
-        pixelOffset: [0, 0]
-      });
-      popups.push(popup)
     }
-    var popupsString = JSON.stringify(popups)
     var pinsString = JSON.stringify(pins)
 
-    sessionStorage.setItem("popups", popupsString)
     sessionStorage.setItem("pins", pinsString)
   })
 }
 
 
 function showData() {
-  console.log("show pins and popups now...")
-  var popups = JSON.parse(sessionStorage.getItem("popups"))
-  var pins = JSON.parse(sessionStorage.getItem("pins"))
-  console.log("Pins is...")
-  console.log(pins)
-  console.log("Popups is...")
-  console.log(popups)
+  //$('#map').load(' #map > *');
+    console.log("show apd pins only...")
+    var pins = JSON.parse(sessionStorage.getItem("pins"))
 
-  map.addEventListener("load", function() {
-    /* Add a customized pin to the map */
-    var layerName = "default-pin";
-    // var pin = new atlas.data.Feature(new atlas.data.Point(mapCenterPosition));
+    map.addEventListener("load", function() {
+      /* Add a customized pin to the map */
+      var layerName = "default-pin";
+      // var pin = new atlas.data.Feature(new atlas.data.Point(mapCenterPosition));
 
+      map.addPins(pins, {
+        fontColor: "#000",
+        fontSize: 14,
+        icon: "pin-red",
+        iconSize: 1,
+        name: layerName,
+        textFont: "SegoeUi-Bold",
+        textOffset: [0, 0],
+        title: ""
+      });
 
-    map.addPins(pins, {
-      fontColor: "#000",
-      fontSize: 14,
-      icon: "pin-red",
-      iconSize: 1,
-      name: layerName,
-      textFont: "SegoeUi-Bold",
-      textOffset: [0, 0],
-      title: ""
-    });
-
-    /* Add an event listener for mouse over */
-    map.addEventListener("mouseover", layerName, function() {
-      /* Open the popup */
-      for (var i = 0; i < popups.length; i++) {
-        var popup = popups[i];
-        popup.open(map);
-      }
-    });
   })
 
 
