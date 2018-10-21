53ff2faa9469def49a54faf963dfa7e22784ed56
Swarn Singh <swarn2099@gmail.com>
2018-10-21 11:12:46 -0500

    Changed port to match VM server

--- src/static/js/home.js
+++ src/static/js/home.js
@@ -3,48 +3,13 @@ function login() {
 }
 function crimeAlert() {
   var description = document.getElementById('description');
+  var location = document.getElementById('location');
+
   var file = document.getElementById('video');
   console.log(file.value);
-  $(function() {
-    var params = {
-        // Request parameters
-        "partition": "{string}",
-        "externalId": "{string}",
-        "callbackUrl": "{string}",
-        "metadata": "{string}",
-        "language": "{string}",
-        "videoUrl": "{string}",
-        "fileName": "{string}",
-        "indexingPreset": "{string}",
-        "streamingPreset": "Default",
-        "linguisticModelId": "{string}",
-        "privacy": "{string}",
-        "externalUrl": "{string}",
-        "assetId": "{string}",
-        "priority": "{string}",
-    };
+  var card = '<div class="row"><div class="col s12 m7"><div class="card"><div class="card-image"><video class="responsive-video" controls><source src="'+file.value+'" type="video/mp4"></video><div class="card-content"><div class="row"><div class="col s6"><h6><b>Description </b></h6></div><div class="col s6 red-text">18% Crime Severity</div></div><p>'+description.value+'</p><h6><b>Location</b></h6><p>'+location.value+'</p><h6><b>Tags</b></h6><p>#bike #theft #man #stolen </p><h6><b>Suspect Description</b></h6><div class="row valign-wrapper"><div class="col s2"><img src="../static/img/thief.jpg" alt="" class="circle responsive-img"></div><div class="col s10"><span class="black-text">Black Jacket, bulk build.</span></div></div></div></div></div></div>';
+  $("#cardList").append(card);
 
-    // var url = "https://api.videoindexer.ai/northeurope/Accounts/c0053fba-cc8d-4204-829e-956248f954c2/Videos?AccessToken?"
-    // $.post(url, function(data){
-    //   console.log(data)
-    // })
 
-    $.ajax({
-        url: "https://api.videoindexer.ai/northeurope/Accounts/c0053fba-cc8d-4204-829e-956248f954c2/Videos?AccessToken?" + $.param(params),
-        beforeSend: function(xhrObj){
-            // Request headers
-            xhrObj.setRequestHeader("Content-Type","multipart/form-data");
-        },
-        type: "POST",
-        // Request body
-        data: "{body}",
-    })
-    .done(function(data) {
-        alert("success");
-    })
-    .fail(function() {
-        alert("error");
-    });
-});
 
 }
